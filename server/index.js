a35bad6af87214df1f9c02be9dcc17e99fc6f926
Muntaha <muntahasayed396@gmail.com>
2025-06-20 21:17:35 +0530

    testing

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use('/api/auth',router)
 
 app.get('/',(req,res)=>{
-  res.json({msg:"waddup!!"})
+  res.json({msg:"waddup amigo!!"})
 })
 
 connectDb().then(()=>app.listen(8000,()=>{console.log("port is running on 8000")}))
